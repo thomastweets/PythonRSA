f1796e22de9118eed6c37fa61cd8d9cd99f0a23e
PiaKS <pk.schroder@student.maastrichtuniversity.nl>
2015-05-13 12:56:41 +0200

    First commit

---
+++ GUI_RSA.py
@@ -0,0 +1,368 @@
+############################
+### GUI for RS analysis ###
+############################
+
+import wx
+import rsa
+import os
+
+class RSA_GUI(wx.Frame):
+    def __init__(self, parent, title):
+        super(RSA_GUI,self).__init__(parent, style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER, title = title, size = (400,275))
+
+        self.InitUI()
+        self.Show(True)
+
+    def InitUI(self):
+
+        ## Creates Status Bar
+        self.CreateStatusBar()
+
+        self.menuBar = wx.MenuBar()
+
+        self.filemenu = wx.Menu()
+        self.helpmenu = wx.Menu()
+
+        self.menuAbout = self.helpmenu.Append(wx.ID_ABOUT, "&About", "Learn more about RSA and how to use this program")
+        self.menuClear = self.filemenu.Append(wx.ID_ANY,"&Clear","Clear data")
+        self.filemenu.AppendSeparator()
+        self.menuExit = self.filemenu.Append(wx.ID_EXIT, "&Exit", "Terminate the program")
+
+        self.menuBar.Append(self.filemenu, "&File")
+        self.menuBar.Append(self.helpmenu, "&Help")
+        self.SetMenuBar(self.menuBar)
+
+        self.Bind(wx.EVT_MENU, self.OnAbout, self.menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, self.menuExit)
+        self.Bind(wx.EVT_MENU, self.OnClear, self.menuClear)
+
+        ## buttons
+
+        self.panel = wx.Panel(self)
+
+        self.main_box = wx.BoxSizer(wx.VERTICAL)
+
+        file_box = wx.BoxSizer(wx.HORIZONTAL)
+        file_button = wx.Button(self.panel, label = 'Select files', size = (90, 30))
+        file_box.Add(file_button)
+        self.file_text = wx.TextCtrl(self.panel)
+        self.file_text.Disable()
+        file_box.Add(self.file_text, proportion = 1, flag = wx.EXPAND | wx.LEFT, border = 5)
+
+        self.main_box.Add(file_box, flag = wx.EXPAND | wx.ALL, border = 10)
+
+        self.main_box.Add((-1,10))
+
+        label_box = wx.BoxSizer(wx.HORIZONTAL)
+        label_button = wx.Button(self.panel, label = 'Conditions', size = (90, 30))
+        label_box.Add(label_button)
+        self.label_text = wx.TextCtrl(self.panel)
+        self.label_text.Disable()
+        label_box.Add(self.label_text, proportion = 1, flag = wx.EXPAND | wx.LEFT, border = 5)
+
+        self.main_box.Add(label_box, flag = wx. EXPAND | wx.RIGHT | wx.LEFT, border = 10)
+
+        self.main_box.Add((-1,30))
+
+        options_box = wx.BoxSizer(wx.HORIZONTAL)
+        options_button = wx.Button(self.panel, label='Options', size = (70, 30))
+        options_box.Add(options_button)
+
+        self.main_box.Add(options_box, flag = wx.ALIGN_RIGHT | wx.RIGHT, border = 10)
+
+        self.main_box.Add((-1,10))
+
+        end_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.go_btn = wx.Button(self.panel, label = 'Go', size = (70, 30))
+        self.go_btn.Disable()
+        end_box.Add(self.go_btn, flag = wx.BOTTOM, border = 5)
+        cancel_btn = wx.Button(self.panel, label = 'Cancel', size = (70, 30))
+        end_box.Add(cancel_btn, flag = wx.LEFT | wx.BOTTOM, border = 5)
+        self.main_box.Add(end_box, flag = wx.ALIGN_RIGHT | wx.RIGHT, border = 10)
+
+        self.panel.SetSizer(self.main_box)
+
+        self.Bind(wx.EVT_BUTTON, self.OnFiles, file_button)
+        self.Bind(wx.EVT_BUTTON, self.conditions, label_button)
+        self.Bind(wx.EVT_BUTTON, self.OnOptions, options_button)
+        self.go_btn.Bind(wx.EVT_BUTTON, self.OnGo)
+        self.Bind(wx.EVT_BUTTON, self.OnCancel, cancel_btn)
+
+        self.labels = []
+        self.files = []
+
+        self.Center()
+
+    def OnOptions(self, e):
+
+        self.new = OptionWindow(parent=None, id=-1)
+        self.new.Show()
+
+    def OnAbout(self, e):
+        dlg = wx.MessageDialog(self, "A small bla bla", "About bla")
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    def OnExit(self, e):
+        self.Close(True)
+
+    def OnClear(self, e):
+        self.files = []
+        self.labels = []
+        self.file_text.ChangeValue(str(''))
+        self.label_text.ChangeValue(str(''))
+
+    def OnFiles(self, event):
+        dialog = wx.FileDialog(self, "Choose files:", os.getcwd(), " ","*.vom", wx.FD_OPEN|wx.FD_MULTIPLE)
+
+        if dialog.ShowModal() == wx.ID_OK:
+            self.paths = dialog.GetPaths()
+            # myfiles contains all the file names
+            for path in self.paths:
+                self.files.append(os.path.basename(path).encode("utf-8"))
+
+        if self.files:
+            self.file_text.ChangeValue(str(', '.join(self.files)))
+            self.go_btn.Enable()
+        dialog.Destroy()
+
+    def conditions(self, event):
+        self.textinput = wx.TextEntryDialog(self, "Type in condition names separated by a white space", "Condition labels")
+
+        if self.textinput.ShowModal() == wx.ID_OK:
+            self.input = self.textinput.GetValue()
+            # labels contains a list of all conditions
+            self.labels = self.input.split()
+            self.labels = [label.encode("utf-8") for label in self.labels]
+
+        if self.labels:
+            self.label_text.ChangeValue(str(', '.join(self.labels)))
+        self.textinput.Destroy()
+
+    def OnGo(self, e):
+        if self.labels == ['Tetris']:
+            import Tetris
+        else:
+            wait = wx.BusyCursor()
+            rsa.RSA(self.paths, self.files, self.labels)
+            del wait
+
+    def OnCancel(self, e):
+        self.Close(True)
+
+class OptionWindow(wx.Frame):
+    def __init__(self, parent, id):
+        wx.Frame.__init__(self, parent, id, 'Options',
+                          style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER ^ wx.MINIMIZE_BOX ^ wx.MAXIMIZE_BOX,
+                          size=(400,500))
+
+        self.InitOpt()
+
+    def InitOpt(self):
+
+        self.panel = wx.Panel(self)
+        self.vbox = wx.BoxSizer(wx.VERTICAL)
+
+        self.vbox.Add((-1,20))
+
+        self.line1 = wx.StaticLine(self.panel, wx.ID_ANY, style=wx.LI_VERTICAL)
+        self.vbox.Add(self.line1, 0, wx.ALL | wx.EXPAND, 5)
+
+        self.vbox.Add((-1,10))
+
+        # Check box: First-order RDMs
+        self.RDM1_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.RDM1_cb = wx.CheckBox(self.panel, label = 'First order RDMs')
+        self.RDM1_cb.SetValue(rsa.output_first)
+        self.RDM1_cb.Bind(wx.EVT_CHECKBOX, self.OnSelectRDM1)
+        self.RDM1_box.Add(self.RDM1_cb)
+        self.vbox.Add(self.RDM1_box, flag = wx.LEFT, border = 10)
+
+        self.vbox.Add((-1,10))
+
+        # Check box: Matrix plots
+        self.mplot1_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.mplot1_box.Add((25,-1))
+        self.mplot1_cb = wx.CheckBox(self.panel, label = 'Matrix plots')
+        self.mplot1_cb.SetValue(rsa.matrix_plot1)
+        self.mplot1_box.Add(self.mplot1_cb)
+        self.vbox.Add(self.mplot1_box, flag = wx.LEFT, border = 10)
+
+        self.vbox.Add((-1,10))
+
+        # Check box: First-order correlations
+        self.correlations1_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.correlations1_box.Add((25,-1))
+        self.correlations1_cb = wx.CheckBox(self.panel, label = 'Correlations')
+        self.correlations1_cb.SetValue(rsa.correlations1)
+        self.correlations1_box.Add(self.correlations1_cb)
+        self.vbox.Add(self.correlations1_box, flag = wx.LEFT, border = 10)
+
+        self.vbox.Add((-1,10))
+
+        # Check box: Scale to maximum distance
+        self.scale_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.scale_box.Add((25,-1))
+        self.scale_cb = wx.CheckBox(self.panel, label='Scale to max')
+        self.scale_cb.SetValue(rsa.scale_to_max)
+        self.scale_box.Add(self.scale_cb)
+        self.vbox.Add(self.scale_box, flag = wx.LEFT, border = 10)
+
+        self.vbox.Add((-1,10))
+
+        # Dropdown menu for distance metric
+        self.drop_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.drop_box.Add((25,-1))
+        self.drop_label = wx.StaticText(self.panel, label = 'Distance metric     ')
+        self.drop_box.Add(self.drop_label)
+
+        self.distances = ['Correlation distance', 'Euclidean distance', 'Absolute activation difference']
+        self.dropdown = wx.ComboBox(self.panel, value = self.distances[rsa.dist_metric-1], choices = self.distances, style=wx.CB_READONLY)
+        self.drop_box.Add(self.dropdown)
+        self.vbox.Add(self.drop_box, flag = wx.LEFT, border = 10)
+
+        self.vbox.Add((-1,20))
+
+        self.line2 = wx.StaticLine(self.panel, wx.ID_ANY, style=wx.LI_VERTICAL)
+        self.vbox.Add(self.line2, 0, wx.ALL | wx.EXPAND, 5)
+
+        self.vbox.Add((-1,10))
+
+        # Check box: Second-order RDM
+        self.RDM2_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.RDM2_cb = wx.CheckBox(self.panel, label = 'Second order RDMs')
+        self.RDM2_cb.SetValue(rsa.output_second)
+        self.RDM2_cb.Bind(wx.EVT_CHECKBOX, self.OnSelectRDM2)
+        self.RDM2_box.Add(self.RDM2_cb)
+        self.vbox.Add(self.RDM2_box, flag = wx.LEFT, border = 10)
+
+        self.vbox.Add((-1,10))
+
+        # Check box: Matrix plots
+        self.mplot2_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.mplot2_box.Add((25,-1))
+        self.mplot2_cb = wx.CheckBox(self.panel, label = 'Matrix plots')
+        self.mplot2_cb.SetValue(rsa.matrix_plot2)
+        self.mplot2_box.Add(self.mplot2_cb)
+        self.vbox.Add(self.mplot2_box, flag = wx.LEFT, border = 10)
+
+        self.vbox.Add((-1,10))
+
+        # Check box: Bar plots
+        self.bplot_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.bplot_box.Add((25,-1))
+        self.bplot_cb = wx.CheckBox(self.panel, label = 'Bar plots')
+        self.bplot_cb.SetValue(rsa.bar_plot)
+        self.bplot_box.Add(self.bplot_cb)
+        self.vbox.Add(self.bplot_box, flag = wx.LEFT, border = 10)
+
+        self.vbox.Add((-1,10))
+
+        # Check box: Second-order correlations
+        self.correlations2_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.correlations2_box.Add((25,-1))
+        self.correlations2_cb = wx.CheckBox(self.panel, label = 'Correlations')
+        self.correlations2_cb.SetValue(rsa.correlations2)
+        self.correlations2_box.Add(self.correlations2_cb)
+        self.vbox.Add(self.correlations2_box, flag = wx.LEFT, border = 10)
+
+        self.vbox.Add((-1,10))
+
+        # Check box: p-values
+        self.p_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.p_box.Add((25,-1))
+        self.p_cb = wx.CheckBox(self.panel, label='p-values')
+        self.p_cb.SetValue(rsa.pvalues)
+        self.p_box.Add(self.p_cb)
+        self.vbox.Add(self.p_box, flag = wx.LEFT, border = 10)
+
+        self.vbox.Add((-1,10))
+
+        # No of permutations SpinControl
+        self.perm_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.perm_box.Add((25,-1))
+        self.perm_label = wx.StaticText(self.panel, label = 'No. of Permutations     ')
+        self.perm_box.Add(self.perm_label)
+
+        self.perm_spin = wx.SpinCtrl(self.panel, value=str(rsa.no_relabelings), min=100, max = 100000)
+        self.perm_box.Add(self.perm_spin, proportion = 1)
+        self.vbox.Add(self.perm_box, flag = wx.LEFT, border = 10)
+
+        self.vbox.Add((-1,10))
+
+        self.line3 = wx.StaticLine(self.panel, wx.ID_ANY, style=wx.LI_VERTICAL)
+        self.vbox.Add(self.line3, 0, wx.ALL | wx.EXPAND, 5)
+
+        self.vbox.Add((-1,50))
+
+        # Dis-/Enable options
+        self.OnSelectRDM1([])
+        self.OnSelectRDM2([])
+
+        # Done and Cancel Buttons
+        self.end_box = wx.BoxSizer(wx.HORIZONTAL)
+        self.done_btn = wx.Button(self.panel, label = 'Done', size = (70, 30))
+        self.done_btn.Bind(wx.EVT_BUTTON, self.OnDone)
+        self.end_box.Add(self.done_btn, flag = wx.BOTTOM, border = 5)
+        self.cancel_btn = wx.Button(self.panel, label = 'Cancel', size = (70, 30))
+        self.cancel_btn.Bind(wx.EVT_BUTTON, self.OnCancel)
+        self.end_box.Add(self.cancel_btn, flag = wx.LEFT | wx.BOTTOM, border = 5)
+        self.vbox.Add(self.end_box, flag = wx.ALIGN_RIGHT | wx.RIGHT, border = 10)
+
+        self.panel.SetSizer(self.vbox)
+
+        self.Center()
+
+
+    def OnSelectRDM1(self,e):
+        if self.RDM1_cb.GetValue():
+            self.mplot1_cb.Enable()
+            self.correlations1_cb.Enable()
+            self.scale_cb.Enable()
+            self.dropdown.Enable()
+        else:
+            self.mplot1_cb.Disable()
+            self.correlations1_cb.Disable()
+            self.scale_cb.Disable()
+            self.dropdown.Disable()
+
+
+    def OnSelectRDM2(self,e):
+        if self.RDM2_cb.GetValue():
+            self.bplot_cb.Enable()
+            self.mplot2_cb.Enable()
+            self.p_cb.Enable()
+            self.correlations2_cb.Enable()
+            self.perm_spin.Enable()
+        else:
+            self.bplot_cb.Disable()
+            self.p_cb.Disable()
+            self.perm_spin.Disable()
+            self.mplot2_cb.Disable()
+            self.correlations2_cb.Disable()
+
+    def OnDone(self,e):
+        rsa.output_first = self.RDM1_cb.GetValue()
+        rsa.output_second = self.RDM2_cb.GetValue()
+        rsa.matrix_plot1 = self.mplot1_cb.GetValue()
+        rsa.matrix_plot2 = self.mplot2_cb.GetValue()
+        rsa.bar_plot = self.bplot_cb.GetValue()
+        rsa.correlations1 = self.correlations1_cb.GetValue()
+        rsa.correlations2 = self.correlations2_cb.GetValue()
+        rsa.pvalues = self.p_cb.GetValue()
+        rsa.scale_to_max = self.scale_cb.GetValue()
+        rsa.no_relabelings = self.perm_spin.GetValue()
+        rsa.dist_metric = self.dropdown.GetSelection()+1
+        self.Close()
+
+    def OnCancel(self,e):
+        self.Close()
+
+
+def main():
+    GUI = wx.App()
+    RSA_GUI(None, 'RSA')
+    GUI.MainLoop()
+
+if __name__ == '__main__':
+    main()
---
+++ rsa.py
@@ -0,0 +1,353 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+################# Representational Similarity Analysis ######################
+################################################################################
+
+import os
+from os.path import dirname, abspath
+import numpy as np
+from scipy.spatial.distance import cdist
+from scipy.stats import spearmanr
+import matplotlib.pyplot as plt
+#import networkx as nx
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import datetime
+
+# Global variables and their default values
+
+matrix_plot1 = True
+matrix_plot2 = False
+bar_plot = False
+correlations1 = False
+correlations2 = False
+pvalues = False
+no_relabelings = 10000
+dist_metric = 1
+output_first = True
+output_second = False
+scale_to_max = False
+
+
+
+def import_data(paths):
+    """ Import header and data matrix from VOM files specified in *args. Returns
+    dictionary DATA containing data set names as keys."""
+
+    # Change path to current working directory
+    # path = dirname(abspath(__file__))
+    # os.chdir(path)
+
+    DATA = dict()
+
+    # Iterate through files and save data in dictionary
+    for set_no, file_name in enumerate(paths):
+
+        header = dict()
+
+        # Read header and store last line
+        with open(file_name, 'r') as file:
+
+            for index, line in enumerate(file):
+                string_list = line.split()
+                item_list = [int(i) if i.isdigit() else i for i in string_list]
+                # For non empty list save first element as key and rest as value in
+                # header dictionary
+                if item_list:
+                    key = item_list.pop(0)
+                    if len(item_list) > 1:
+                        header[key] = item_list
+                    else: header[key] = item_list.pop()
+                # Use 'NrOfVoxels' as indicator for the end of the header
+                if 'NrOfVoxels:' in line:
+                    break
+
+        header_end = index + 1
+        # Read data into array
+        data = np.loadtxt(file_name, skiprows = header_end)
+
+        # Save data set in DATA dictionary
+        key = "data_set_" + str(set_no + 1)
+        DATA[key] = {'header': header, 'data': data}
+
+    return DATA
+
+
+def extract_data(DATA):
+    """ Get subject data from data matrices in DATA. One matrix per subject,
+    one column per condition. """
+    # Extracts those columns in data that contain measurements (excluding voxel coordinates)
+    data = []
+    for i in range(1,len(DATA)+1):
+        data.append(DATA['data_set_' + str(i)]['data'][:,3:])
+    return data
+
+
+def first_order_rdm(condition_data):
+    """ Return Specified distance matrices (1 = Pearson correlation,
+    2 = Euclidian distance, 3 = Absolute activation difference)  of data in
+    input arrays. One array per area/subject/method/...
+    Number of rows/columns = number of conditions = number of columns in each
+    matrix in condition_data"""
+
+    RDMs = list()
+
+    # Iterate through matrices in condition_data and save one RDM per matrix
+    for i in range(len(condition_data)):
+        #RDMs.append(cdist(condition_data[i], condition_data[i], 'correlation'))
+
+        if dist_metric == 1:
+            # Use correlation distance
+            RDM = 1-np.corrcoef(condition_data[i],rowvar=0)
+
+        elif dist_metric == 2:
+            # Use Eucledian distance
+            RDM = cdist(condition_data[i].T,condition_data[i].T,'euclidean')
+
+            # Or "by hand":
+            '''
+            no_cond = int(np.size(condition_data[i],axis=1))
+            RDM = np.zeros((no_cond,no_cond))
+
+            # Iterate through upper triangle of RDM
+            for m in range(no_cond):
+                for n in range(m+1,no_cond):
+                    RDM[m,n] = np.linalg.norm(condition_data[i][:,m]-condition_data[i][:,n])
+
+            # Mirror along diagonal
+            RDM = RDM + RDM.T
+            '''
+
+        elif dist_metric == 3:
+            # Use absolute activation difference
+            means = np.mean(condition_data[i], axis=0) # Determine mean activation per condition
+            m, n = np.meshgrid(means,means) # Create all possible combinations
+            RDM = abs(m-n) # Calculate difference for each combination
+
+        RDMs.append(RDM)
+
+    return RDMs
+
+
+def get_pvalue(matrix1, matrix2):
+    """ Randomize condition labels to test significance """
+
+
+    order = range(0,len(matrix2))
+    dist = np.zeros(no_relabelings)
+
+    # First, determine actual correlation
+    flat1 = matrix1.flatten(1).transpose()
+    flat2 = matrix2.flatten(1).transpose()
+    corr = spearmanr(flat1,flat2)[0]
+
+    # Relabel N times to obtain distribution of correlations
+    for i in range(0,no_relabelings):
+        np.random.shuffle(order)
+        dummy = matrix2.take(order, axis=1).take(order, axis=0)
+        flat2 = dummy.flatten(1).transpose()
+        dist[i] = spearmanr(flat1,flat2)[0]
+
+    # Determine p value of actual correlation from distribution
+    p = float((dist >= corr).sum()) / len(dist)
+
+    # Mit dieser Methode brauch man mindestens 4 conditions, also 4!=24 mögliche
+    # Reihenfolgen um auf p < 0.05 zu kommen. Nicht gut!
+
+    return p
+
+
+def second_order_rdm(RDMs):
+    """ Returns representational dissimilarity matrix computed with Spearman rank correlations
+    between variable number of equally sized input matrices. """
+
+    # Flatten input matrices
+    flat = [m.flatten(1) for m in RDMs]
+
+
+    flat = np.array(flat).transpose()
+
+    # Compute Spearman rank correlation matrix
+    c_matrix = spearmanr(flat)[0]
+
+    # In case only two conditions are compared, spearmanr returns single correlation
+    # coefficient and c_matrix has to be built manually
+    if not(isinstance(c_matrix, np.ndarray)):
+        c_matrix = np.array([[1,c_matrix],[c_matrix,1]])
+
+    # Compute RDM (distance matrix) with correlation distance: 1 - correlation
+    RDM = np.ones(c_matrix.shape) - c_matrix
+
+
+
+    if pvalues or bar_plot:
+        # Determine significance of second order RDM
+        p_values = np.zeros(RDM.shape)
+
+        # Iterate through pvalue matrix and fill in p-values but only for upper
+        # triangle to improve performance
+        for i in range(0,len(p_values)):
+            for j in range(i,len(p_values)):
+                p_values[i,j] = get_pvalue(RDMs[i], RDMs[j])
+
+        # mirror matrix to obtain all p-values
+        p_values = p_values + np.triu(p_values,1).T
+    else:
+        p_values = []
+
+    return [RDM, p_values]
+
+
+def plot_RDM(RDMs, labels, names, fig):
+
+    # Determine optimal arrangement for plots
+    rows = int(np.sqrt(len(RDMs)))
+    columns = int(np.ceil(len(RDMs)/float(rows)))
+
+    ticks = np.arange(len(labels))
+
+    # Use maximum value in RDMs for scaling if desired
+    dist_max = np.max(np.array(RDMs))
+
+    f = plt.figure(fig)
+
+    # New: add_subplot instead of subplots to control figure instance
+    for index in np.arange(len(RDMs)):
+
+        ax = f.add_subplot(rows,columns,index+1, xticklabels = labels, yticklabels = labels, xticks = ticks, yticks = ticks)
+
+        if scale_to_max:
+            im = ax.imshow(RDMs[index], interpolation = 'none', cmap = 'jet', vmin = 0, vmax = dist_max)
+        else:
+            im = ax.imshow(RDMs[index], interpolation = 'none', cmap = 'jet')
+
+        ax.xaxis.tick_top()
+        ax.set_title(names[index], y = 1.08)
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        dist_max = np.max(RDMs[index])
+        cbar = plt.colorbar(im, ticks=[0, dist_max], cax=cax)
+        cbar.ax.set_ylabel('Dissimilarity')
+        cbar.ax.set_yticklabels(['0', str(np.around(dist_max,decimals=2))])
+
+    f.subplots_adjust(hspace=0.1, wspace=0.3)
+
+
+    if fig == 0:
+        if dist_metric == 1:
+            f.suptitle('First order distance metric: Correlation distance', y=0.9, fontsize=18)
+        elif dist_metric == 2:
+            f.suptitle('First order distance metric: Euclidean distance', y=0.9, fontsize=18)
+        elif dist_metric == 3:
+            f.suptitle('First order distance metric: Absolute activation difference', y=0.9, fontsize=18)
+
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
+
+
+def plot_bars(RDM, pvalues, names):
+
+    length = len(RDM)
+    max = np.max(RDM)
+
+    f = plt.figure(3)
+
+    for index in np.arange(length):
+
+        xticks = np.arange(length-1)+1
+
+        d_values = RDM[index,:]
+        plot_dvalues = d_values[d_values != 0]
+
+        p_values = pvalues[index,:]
+        plot_pvalues = p_values[d_values != 0]
+
+        plot_names = np.array(names)[d_values != 0]
+
+        sort = np.argsort(plot_pvalues)
+
+        ax = f.add_subplot(1,length, index+1, xticks = xticks, xticklabels = plot_pvalues[sort])
+        ax.set_ylabel('Correlation distance (1-Spearman rank correlation)')
+
+        ax.bar(xticks, plot_dvalues[sort], 0.5, align = 'center')
+        plt.axis([0.5, length-0.5, 0, max+max*0.1])
+
+        ax.set_title(names[index])
+
+        for ind in np.arange(length-1):
+            ax.text(xticks[ind], max*0.1, plot_names[sort][ind],
+                 rotation='vertical', horizontalalignment='center',
+                 backgroundcolor='w', color='k', visible=True)
+
+
+    f.subplots_adjust(hspace=0.1, wspace=0.3)
+
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
+
+#def MDS(RDM, labels)
+#    G = nx.from_numpy_matrix(RDM)
+#    nx.draw(G)
+
+
+def RSA(paths, files, labels):
+    ''' Imports input files, extracts relevant data, computes first and second
+    order RDMs and plots them'''
+
+    data = import_data(paths)
+    data = extract_data(data)
+    withinRDMs = first_order_rdm(data)
+
+    if output_second:
+        betweenRDM = second_order_rdm(withinRDMs)
+
+    names = [file[0:-4] for file in files]
+
+    # Numerical output
+    if (output_first and correlations1) or (output_second and (correlations2 or pvalues)):
+
+        now = datetime.datetime.now()
+        filename = "RSA_output%d-%d-%d-%d-%d-%d.txt" % (now.day, now.month, now.year, now.hour,
+                   now.minute, now.second)
+
+        fid = open(filename, 'w')
+
+        fid.write("Representational similarity analysis\n\n")
+        fid.write("Areas: "+str(names)+"\n")
+        fid.write("Conditions: "+str(labels)+"\n\n\n")
+
+
+        if output_first and correlations1:
+
+            distances = {1:'Correlation distance', 2:'Euclidean distance', 3:'Absolute activation difference'}
+
+            fid.write("Dissimilarity between conditions: "+distances[dist_metric]+"\n\n")
+            for ind in np.arange(len(withinRDMs)):
+                np.savetxt(fid, withinRDMs[ind], fmt='%.4f' , header="\n"+names[ind]+"\n")
+                fid.write("\n")
+            fid.write("\n")
+
+        if output_second and correlations2:
+
+            fid.write("Dissimilarity between areas: 1-Spearman rank correlation\n\n")
+            np.savetxt(fid, betweenRDM[0], fmt='%.4f')
+            fid.write("\n\n")
+
+        if output_second and pvalues:
+
+            fid.write("Statistical significance of Dissimilarity between areas.\nP-values are obtained by random relabeling of conditions.\n\n")
+            np.savetxt(fid, betweenRDM[1], fmt='%.4f')
+            fid.write("\n\n")
+
+
+    # Plots
+    if matrix_plot1:
+        plot_RDM(withinRDMs, labels, names, 1)
+    if output_second and matrix_plot2:
+        plot_RDM([betweenRDM[0]], names, ['Second order RDM'], 2)
+    if output_second and bar_plot:
+        plot_bars(betweenRDM[0], betweenRDM[1], names)
+    if matrix_plot1 or matrix_plot2 or bar_plot:
+        #mng = plt.get_current_fig_manager()
+        #mng.window.showMaximized()
+        plt.show()
